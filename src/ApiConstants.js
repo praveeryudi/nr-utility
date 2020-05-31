cf62ff15199314594ea3c7e8def6745862d00eb2
Praveer Das <praveer.das@dish.com>
2020-05-31 22:24:25 +0530

    new feature - maintenance

--- src/ApiConstants.js
+++ src/ApiConstants.js
@@ -4,3 +4,5 @@ export const FETCH_ALL_EMPLOYEES = `https://${host}/employees/all`;
 export const FETCH_ALL_JOBS = `https://${host}/jobs/all`;
 export const FETCH_ALL_MANAGERS = `https://${host}/managers/all`;
 export const FETCH_EMPLOYEE_JOB_HISTORY = `https://${host}/api/fetch/employee/jobHistory/`;
+
+export const LOAD_MAINTENANCE_MASTER_DATA = `https://${host}/maintenance/lookup`;
