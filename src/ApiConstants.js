2e959b4d07d57b42f50f62e9233b92c056c33702
Praveer <praveeryudi@gmail.com>
2020-06-07 13:33:17 +0530

    changed the host

--- src/ApiConstants.js
+++ src/ApiConstants.js
@@ -6,11 +6,11 @@ export const FETCH_ALL_JOBS = `https://${host}/jobs/all`;
 export const FETCH_ALL_MANAGERS = `https://${host}/managers/all`;
 export const FETCH_EMPLOYEE_JOB_HISTORY = `https://${host}/api/fetch/employee/jobHistory/`;
 
-export const LOAD_MAINTENANCE_MASTER_DATA = `http://${host}/maintenance/lookup`;
-export const FETCH_ALL_TRANSACTIONS = `http://${host}/maintenance/transactions/all`;
-export const DELETE_TRANSACTION = `http://${host}/maintenance/deleteTxn`;
+export const LOAD_MAINTENANCE_MASTER_DATA = `https://${host}/maintenance/lookup`;
+export const FETCH_ALL_TRANSACTIONS = `https://${host}/maintenance/transactions/all`;
+export const DELETE_TRANSACTION = `https://${host}/maintenance/deleteTxn`;
 
-export const FETCH_INDIVIDUAL_FLAT_DATA = `http://${host}/maintenance/flat/`;
-export const SAVE_MAINTENANCE_DATA = `http://${host}/maintenance/addMaintenance`;
-export const SAVE_MAINTENANCE_BATCH_DATA = `http://${host}/maintenance/addMaintenanceBatch`;
-export const PENDING_MAINTENANCE_DATA = `http://${host}/maintenance/pending/`;
+export const FETCH_INDIVIDUAL_FLAT_DATA = `https://${host}/maintenance/flat/`;
+export const SAVE_MAINTENANCE_DATA = `https://${host}/maintenance/addMaintenance`;
+export const SAVE_MAINTENANCE_BATCH_DATA = `https://${host}/maintenance/addMaintenanceBatch`;
+export const PENDING_MAINTENANCE_DATA = `https://${host}/maintenance/pending/`;
